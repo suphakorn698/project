c3f04cb4db7f8febdb65a6977e2afe12d257c3dc
suphakorn698 <suphakorn.epn@gmail.com>
2025-03-08 13:51:27 +0700

    Add files via upload

---
+++ RPS.sol
@@ -0,0 +1,58 @@
+
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract RPS {
+    uint public numPlayer = 0;
+    uint public reward = 0;
+    mapping (address => uint) public player_choice; // 0 - Rock, 1 - Paper , 2 - Scissors
+    mapping(address => bool) public player_not_played;
+    address[] public players;
+
+    uint public numInput = 0;
+
+    function addPlayer() public payable {
+        require(numPlayer < 2);
+        if (numPlayer > 0) {
+            require(msg.sender != players[0]);
+        }
+        require(msg.value == 1 ether);
+        reward += msg.value;
+        player_not_played[msg.sender] = true;
+        players.push(msg.sender);
+        numPlayer++;
+    }
+
+    function input(uint choice) public  {
+        require(numPlayer == 2);
+        require(player_not_played[msg.sender]);
+        require(choice == 0 || choice == 1 || choice == 2);
+        player_choice[msg.sender] = choice;
+        player_not_played[msg.sender] = false;
+        numInput++;
+        if (numInput == 2) {
+            _checkWinnerAndPay();
+        }
+    }
+
+    function _checkWinnerAndPay() private {
+        uint p0Choice = player_choice[players[0]];
+        uint p1Choice = player_choice[players[1]];
+        address payable account0 = payable(players[0]);
+        address payable account1 = payable(players[1]);
+        if ((p0Choice + 1) % 3 == p1Choice) {
+            // to pay player[1]
+            account1.transfer(reward);
+        }
+        else if ((p1Choice + 1) % 3 == p0Choice) {
+            // to pay player[0]
+            account0.transfer(reward);    
+        }
+        else {
+            // to split reward
+            account0.transfer(reward / 2);
+            account1.transfer(reward / 2);
+        }
+    }
+}
